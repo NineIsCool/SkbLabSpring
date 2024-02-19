19679d94685527d47d10fcfba41fd67fb0f093d1
Alexeev Nikita <nakinaki243@gmail.com>
2024-02-19 16:20:12 +0500

    Initial commit

---
+++ src/main/java/com/example/skblabspring/SkbLabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.skblabspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbLabSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkbLabSpringApplication.class, args);
+    }
+
+}
